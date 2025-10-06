38f55aa6fe5287929b620f2d194071096c42bec5
Chen Ran Wong <>
2025-10-06 22:50:01 +0800

    新增說明

--- src/app/api/post/list/route.ts
+++ src/app/api/post/list/route.ts
@@ -16,11 +16,11 @@ export const GET = withApiHandler(async (request: NextRequest) => {
   const collection = db.collection("posts");
   const total = await collection.countDocuments();
   const posts = await collection
-    .find({})
-    .skip(skip)
-    .limit(Number(limit))
-    .sort({ createdAt: -1 })
-    .toArray();
+    .find({}) // find({}) 是查詢（query）指令： {} → 查全部（沒有條件）
+    .skip(skip) // skip() 表示要略過前面多少筆資料。
+    .limit(Number(limit)) // 表示最多取幾筆資料。
+    .sort({ createdAt: -1 }) // 表示排序方式 -1 是新到舊
+    .toArray(); //find() 回傳的是游標（Cursor），它像一個查詢結果的串流（stream）。    .toArray() 會把結果全部「收集」成一個陣列：
 
   return Response.json(
     success({
